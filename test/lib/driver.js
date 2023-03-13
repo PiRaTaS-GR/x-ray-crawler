fa2fe0461cf4d3c2140b8235b8378cc2503fd8c8
thegu5 <58223632+thegu5@users.noreply.github.com>
2023-03-13 15:22:56 -0400

    use standard style, update deps

--- test/lib/driver.js
+++ test/lib/driver.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 
-const superagent = require('superagent');
+const superagent = require('superagent')
 
-const agent = superagent.agent({});
+const agent = superagent.agent({})
 
-module.exports = function http(ctx, fn) {
-    agent
-        .get(ctx.url)
-        .set(ctx.headers)
-        .end(function(err, res) {
-            if (err) {
-                return fn(err);
-            }
-            ctx.status = res.status;
-            ctx.set(res.headers);
-            ctx.body = 'application/json' == ctx.type ? res.body : res.text
-            // update the URL if there were redirects
-            ctx.url = res.redirects.length ? res.redirects.pop() : ctx.url
-            return fn(null, ctx);
-        });
-};
+module.exports = function http (ctx, fn) {
+  agent
+    .get(ctx.url)
+    .set(ctx.headers)
+    .end(function (err, res) {
+      if (err) {
+        return fn(err)
+      }
+      ctx.status = res.status
+      ctx.set(res.headers)
+      ctx.body = ctx.type === 'application/json' ? res.body : res.text
+      // update the URL if there were redirects
+      ctx.url = res.redirects.length ? res.redirects.pop() : ctx.url
+      return fn(null, ctx)
+    })
+}
